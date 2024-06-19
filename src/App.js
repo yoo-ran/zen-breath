c56b5355c0122614f43725e9274098cd9eaafb8f
YooranKim <yuranm@naver.com>
2024-06-19 09:49:24 -0700

    upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import Layout from './components/layout/layout.js';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Header from './components/Header';
+import Meditation from './pages/MeditationPage';
+import Music from './pages/MusicPage';
+import Breath from './pages/BreathPage';
+import Setting from './pages/Settings';
+import Profile from './pages/Profile';
 
-function App() {
-  return (
-    <Layout>
-    </Layout>
-    
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/meditation" element={<Meditation />} />
+              <Route path="/music" element={<Music />} />
+              <Route path="/breath" element={<Breath />} />
+              <Route element={<Profile />} />
+              <Route path="/setting" element={<Setting />} />
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+// src/components/Header.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouseChimney, faFan, faMusic, faClover, faGear, faWind } from '@fortawesome/free-solid-svg-icons'
+import { faAndroid, faApple} from '@fortawesome/free-brands-svg-icons'
+
+import logo from "../assets/logo-02.svg"
+
+const Header = () => {
+    return (
+        <header className='md:w-2/12 bg-purple flex flex-col justify-between drop-shadow-xl'>
+            <div className={`flex flex-col items-center justify-center md:h-1/4 md:gap-y-4`}>
+                <img src={logo} className='md:w-8'/>
+                <h1 className={`font-head font-bold md:text-xl`}>ZenBreath</h1>
+                <button className={`text-blue bg-white drop-shadow-xl rounded-full md:py-1 md:w-1/2`}>60% OFF</button>
+            </div>
+            <nav className={`md:h-1/2 flex flex-col items-start justify-center`}>
+                <ul className={`w-full h-full flex flex-col justify-between md:pl-4`}>
+                    <li className={` w-full h-full flex items-center`}><Link to="/" className={`flex items-center md:gap-x-4`}><FontAwesomeIcon icon={faHouseChimney} /> Home</Link></li>
+                    <li className={` w-full h-full flex items-center`}><Link to="/meditation" className={`flex items-center md:gap-x-4`}><FontAwesomeIcon icon={faFan} /> Meditation</Link></li>
+                    <li className={` w-full h-full flex items-center`}><Link to="/Music" className={`flex items-center md:gap-x-4`}><FontAwesomeIcon icon={faMusic} /> Music</Link></li>
+                    <li className={` w-full h-full flex items-center`}><Link to="/breathe" className={`flex items-center md:gap-x-4`}><FontAwesomeIcon icon={faWind} /> Breathe</Link></li>
+                    <li className={` w-full h-full flex items-center`}><Link to="/profile" className={`flex items-center md:gap-x-4`}><FontAwesomeIcon icon={faClover} /> Profile</Link></li>
+                    <li className={` w-full h-full flex items-center`}><Link to="/setting" className={`flex items-center md:gap-x-4`}><FontAwesomeIcon icon={faGear} /> Setting</Link></li>
+                </ul>
+            </nav>
+            <footer className={`md:h-1/4 flex flex-col items-center justify-center text-footer md:gap-y-4 `}>
+                    <div className='flex md:gap-x-4 md:text-xl'>
+                        <FontAwesomeIcon icon={faAndroid} />
+                        <FontAwesomeIcon icon={faApple} />
+                    </div>
+                    <p>Mobile App</p>
+            </footer>
+        </header>
+    );
+};
+
+export default Header;
